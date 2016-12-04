c6face581f40702ee0a6ae874cc18f1073c9f6c8
jalawren <jalawren@gmail.com>
2016-12-04 17:07:31 -0500

    resources fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                             <li><a href="{{ $url = route('philosophy') }}">health philosophy</a></li>
                             <li><a href="{{ $url = route('contact') }}">contact</a></li>
                             <li><a href="/blog">blog</a></li>
-                            <li><a href="{{ $url = route('resources') }}">resources</a></li>
+                            <li><a href="/resources">resources</a></li>
                         </ul>
                     </div>
                 </div>
